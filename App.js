624d49130ae338c97c24d40246990f59102b903c
Kawin Chuangyanyong <90861008+No-111@users.noreply.github.com>
2022-09-28 11:14:48 +0700

    commit 11:15 09/28/2022

--- App.js
+++ App.js
@@ -1,44 +1,103 @@
-import { View, Text , ActivityIndicator , FlatList } from 'react-native'
-import React , {useState,useEffect}from 'react'
+import { Text, View, Button, StyleSheet, SafeAreaView, Image } from 'react-native';
+import React from 'react';
+
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem
+} from '@react-navigation/drawer';
+
+import rtlogo from './assets/react_logo.png';
+
+import Homescreen from './screens/Homescreen';
+
+import ProductScreen from './ProductScreen'
 
 
-const App = () => {
-
-  const [isLoading,setLoading] = useState(true);
-  const [data,setData] = useState([]);
-
-  const getMovie = async ()=>{
-    try {
-      const response = await fetch('https://reactnative.dev/movies.json');
-      const json = await response.json();
-      setData(json.movies)
-    } catch (error) {
-      alert(error.message);
-    }finally{
-      setLoading(false);
-    }
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'rgb(255,45,85)'
   }
+}
 
-  useEffect(()=>{
-    getMovie();
-  },[])
-
+function FeedScreen({ navigation }) {
   return (
-    <View style={{flex:1,padding:20,}}>
-      {isLoading 
-      ?<ActivityIndicator/>
-      :(
-        <FlatList
-        data={data}
-        keyExtractor ={({id},index)=>id}
-        renderItem={({item})=>(
-          <Text>{item.title},{item.releaseYear}</Text>
-        )}
-        />
-      )
-      }
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        Feed Screen
+      </Text>
+      <Button title='Open Drawer' onPress={() => navigation.openDrawer()} />
+      <Button title='Toggle Drawer' onPress={() => navigation.toggleDrawer()} />
+
     </View>
   )
 }
 
+function ArticleScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        Product Screen
+      </Text>
+    </View>
+  )
+}
+
+function CustomDrawerContent(props) {
+  return (
+    <SafeAreaView style={{flex:1}}>
+      <DrawerContentScrollView {...props}>
+        <Image style={styles.sideMenuProfileIcon} source={rtlogo} />
+        <DrawerItemList {...props} />
+        {/* <DrawerItem label='Close Drawer' onPress={() => props.navigation.closeDrawer()} /> */}
+        {/* <DrawerItem label='Toggle Drawer' onPress={()=> props.navigation.toggleDrawer()}/> */}
+      </DrawerContentScrollView>
+    </SafeAreaView>
+  );
+
+}
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props}
+      />}
+      screenOptions={{
+        drawerStyle: {
+          width: 240
+        }
+      }}
+    >
+      <Drawer.Screen name='Home' component={Homescreen} />
+      <Drawer.Screen name='Product' component={ProductScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer />
+    </NavigationContainer>
+  )
+}
+
 export default App
+
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+    resizeMode: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignSelf: 'center',
+  },
+})
